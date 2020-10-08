22348aebe7654ef5ef9aa4e2034f7a05af5a5269
Shubham Lad <shubhamlad1001@gmail.com>
2020-10-09 04:25:28 +0530

    react build

---
+++ client/build/precache-manifest.b9e2b1b42cb2b7134105af27188958e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d43968869f30585959d06c4c08935975",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77fd5935b9cc7c049d84",
+    "url": "/static/css/main.2090f721.chunk.css"
+  },
+  {
+    "revision": "9b11024b5448d047cec0",
+    "url": "/static/js/2.6852df64.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6852df64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77fd5935b9cc7c049d84",
+    "url": "/static/js/main.87541bb1.chunk.js"
+  },
+  {
+    "revision": "cea8bdde12526d1e2779",
+    "url": "/static/js/runtime-main.e0b50fc5.js"
+  }
+]);
